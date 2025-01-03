e81aa8eb47ed70b8594c4474201edb6dd4a74e96
mahsa hosseini <84369160+misshosseini@users.noreply.github.com>
2025-01-03 23:16:23 +0000

    Add Doctor and Notification models with fields for doctor details, specialization, and notification types

--- doctor/models.py
+++ doctor/models.py
@@ -9,16 +9,16 @@ NOTIFICATION_TYPE = {
 }
 
 class Doctor(models.Model):
-    user = models.OneToOneField(userauths_models.models.User, on_delete=models.CASCADE)
-    image = models.FileField(upload_to="images", null=True, blank=True)
+    user = models.OneToOneField(userauths_models.User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100, null=True, blank=True)
+    image = models.FileField(upload_to="images", null=True, blank=True)
     mobile = models.CharField(max_length=100, null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
     bio = models.CharField(max_length=100, null=True, blank=True)
     specialization = models.CharField(max_length=100, null=True, blank=True)
-    qualification = models.CharField(max_length=100, null=True, blank=True)
+    qualifications = models.CharField(max_length=100, null=True, blank=True)
     years_of_experience = models.CharField(max_length=100, null=True, blank=True)
-    next_available_appointment_data = models.CharField(max_length=100, null=True, blank=True)
+    next_available_appointment_data = models.DateTimeField(default=timezone.now, null=True, blank=True)
 
     def __str__(self):
         return f" دکتر {self.full_name}"
